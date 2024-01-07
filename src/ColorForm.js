15d8cfe033504770f7f6c5c528a2e65aca5c3a56
Jackury13 <143567012+Jackury13@users.noreply.github.com>
2024-01-07 09:19:59 -0800

    initial commit/finished

---
+++ src/ColorForm.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+
+function ColorForm(props){
+    let [input, setInput] = useState('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        props.addColor(input)
+        setInput('')
+    }
+    
+    const clear = () => {
+        setInput('')
+    }
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input value={input} type="text" onChange={(e) => setInput(e.target.value)} />
+                <button type="submit">Add a Color!</button>
+                <button onClick={clear}>Clear</button>
+            </form>
+                    
+        </div>
+    )
+}
+export default ColorForm;
